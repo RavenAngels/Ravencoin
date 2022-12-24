5cf254bab1d31e2cd9031c0591251d588d3d6c04
lsji07 <39772657+lsji07@users.noreply.github.com>
2022-12-24 14:36:07 +0000

    Build fix

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -43,10 +43,11 @@ const std::string CLIENT_NAME("Ravencoin");
 #include "build.h"
 #endif
 
-//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. $Format:%n#define GIT_ARCHIVE 1$
+//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
+#define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "$Format:%h$"
-#define GIT_COMMIT_DATE "$Format:%cD$"
+#define GIT_COMMIT_ID "2e9ae19e8f"
+#define GIT_COMMIT_DATE "Sat, 24 Dec 2022 18:25:39 +0800"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
